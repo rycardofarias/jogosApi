495132a089e06e7f7ff6a510d3ad2c032a41042c
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-07-12 01:26:57 -0300

    Initial commit

---
+++ src/main/java/com/example/jogosApi/JogosApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jogosApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jogosApi/JogosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jogosApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
